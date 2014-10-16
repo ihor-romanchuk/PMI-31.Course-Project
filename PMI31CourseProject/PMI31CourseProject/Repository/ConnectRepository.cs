cae85451358d18c3ca06d4e06e5f5a8428225e63
lucky09 <kopylets2580@gmail.com>
2014-10-16 14:08:21 +0300

    DAL main project
    main files for work with project.

---
+++ PMI31CourseProject/PMI31CourseProject/Repository/ConnectRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMI31CourseProject.Repository
+{
+    class ConnectRepository
+    {
+    }
+}
