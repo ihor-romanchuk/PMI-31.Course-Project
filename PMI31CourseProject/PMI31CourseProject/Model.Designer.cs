1c7c1fd22ce107079d24786bf0dc534b7eabc58b
Bodik69 <bsmachylo86@gmail.com>
2014-12-09 16:39:05 +0200

    видалив стару модель даних

--- PMI31CourseProject/PMI31CourseProject/Model.Designer.cs
+++
@@ -1,4 +0,0 @@
-﻿// Default code generation is disabled for model 'D:\games\everything\GitHub\PMI-31.Course-Project\PMI31CourseProject\PMI31CourseProject\Model.edmx'. 
-// To enable default code generation, change the value of the 'Code Generation Strategy' designer
-// property to an alternate value. This property is available in the Properties Window when the model is
-// open in the designer.
