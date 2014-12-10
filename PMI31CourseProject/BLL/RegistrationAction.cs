651c61a0a58ec3c552e466d764922c034d1f9c4b
Kopylets Misha <kopylets2580@gmail.com>
2014-12-10 21:44:36 +0200

    correct registration status

--- PMI31CourseProject/BLL/RegistrationAction.cs
+++ PMI31CourseProject/BLL/RegistrationAction.cs
@@ -37,12 +37,85 @@ namespace BLL
         /// </summary>
         public Role role { get; set; }
 
-        
+        /// <summary>
+        /// check role is graduate
+        /// </summary>
+        /// <returns>rue if role is graduate</returns>
+        public bool CheckRoleIsGraduate()
+        {
+            if (this.role == Role.Graduate)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// check role is Lecturer
+        /// </summary>
+        /// <returns>true if role is lecturer</returns>
+        public bool CheckRoleIsLecturer()
+        {
+            if (this.role == Role.Lecturer)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// set role for user
+        /// </summary>
+        /// <param name="roleName">role for user</param>
+        public void SetRoleForUser(ref string roleName)
+        {
+            if (CheckRoleIsGraduate())
+            {
+                roleName = "graduate";
+            }
+            else
+            {
+                if (CheckRoleIsLecturer())
+                {
+                    roleName = "lecturer";
+                }
+            }
+        }
+
+        /// <summary>
+        /// set registration
+        /// </summary>
+        /// <returns>set registration status for user</returns>
+        public RegistrationStatus SetRegistrationStatus()
+        {
+            if (CheckRoleIsGraduate())
+            {
+                return RegistrationStatus.RegistratedGraduate;
+            }
+            else
+            {
+                if (CheckRoleIsLecturer())
+                {
+                    return RegistrationStatus.RegistratedLecturer;
+                }
+                else
+                {
+                    return RegistrationStatus.Failed;
+                }
+            }
+        }
+
         /// <summary>
         /// This method Registration Check users
         /// </summary>
         /// <param name="users">Users to check.</param>
-        /// <returns>Status of registration.</returns>
+        /// <returns>Status of registration</returns>
         public RegistrationStatus RegistrationCheck(ManageUsers users)
         {
             User regUser;
@@ -58,30 +131,12 @@ namespace BLL
                 regUser.Password = Security.HashPassword(password);
                 regUser.FullName = fullName;
                 string roleName = string.Empty;
-                if (this.role == Role.Graduate)
-                {
-                    roleName = "graduate";
-                }
-                if (this.role == Role.Lecturer)
-                {
-                    roleName = "lecturer";
-                }
+                this.SetRoleForUser(ref roleName);
                 regUser.Role = roleName;
                 users.AddUser(regUser);
                 regUser.IsRegistered = true;
                 regUser.UserInfo = new UserInfo();
-                if (this.role == Role.Graduate)
-                {
-                    return RegistrationStatus.RegistratedGraduate;
-                }
-                if (this.role == Role.Lecturer)
-                {
-                    return RegistrationStatus.RegistratedLecturer;
-                }
-                else
-                {
-                    return RegistrationStatus.Failed;
-                }
+                return SetRegistrationStatus();
             }
         }
     }
