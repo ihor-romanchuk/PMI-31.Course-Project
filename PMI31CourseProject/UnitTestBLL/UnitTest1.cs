e3882cf51b91ac6e588c571144634fbf4021adaa
molochiy <molochiy@gmail.com>
2014-12-10 04:09:47 +0200

    Unit Tests For Class Security

--- PMI31CourseProject/UnitTestBLL/UnitTest1.cs
+++ PMI31CourseProject/UnitTestBLL/UnitTest1.cs
@@ -9,46 +9,79 @@ namespace BLLUnitTests
     [TestClass]
     public class UnitTestsForLoginAction
     {
-        [TestMethod]
-        [ExpectedException(typeof(InvalidOperationException))]
-        public void TestAuthenticationCheckMethodForNoUser()
-        {
-            ManageUsers testUser = new ManageUsers();
+        //[TestMethod]
+        //[ExpectedException(typeof(InvalidOperationException))]
+        //public void TestAuthenticationCheckMethodForNoUser()
+        //{
+        //    ManageUsers testUser = new ManageUsers();
 
-            LoginAction testLoginAction = new LoginAction("isAbcenteHere", "111111");
-            testLoginAction.AuthenticationCheck(testUser);
+        //    LoginAction testLoginAction = new LoginAction("isAbcenteHere", "111111");
+        //    testLoginAction.AuthenticationCheck(testUser);
+        //}
+
+        //[TestMethod]
+        //public void Test()
+        //{
+
+        //    ManageUsers testUser = new ManageUsers();
+
+        //    LoginAction testLoginAction = new LoginAction("isAbcenteHere", "111111");
+        //    testLoginAction.AuthenticationCheck(testUser);
+        //    Assert.IsTrue(testLoginAction.AuthenticationCheck(testUser).Equals(AuthenticationStatus.NoUser));
+        //    ManageUsers testUser = new ManageUsers();
+        //    UserOfSite loggingUser = testUser.GetById("qwer");
+        //    testUser.AddUser(loggingUser);
+        //    RegistrationAction user = new RegistrationAction();
+        //    user.username = "qwer";
+        //    user.password = "11111";
+        //    user.email = "test@gmail.com";
+        //    user.role = Role.Graduate;
+        //    LoginAction testLoginAction = new LoginAction("qwer", "111111");
+        //    Assert.IsFalse(testLoginAction.AuthenticationCheck(testUser).Equals(AuthenticationStatus.NoUser));
+        //}
+
+        //[TestMethod]
+        //[ExpectedException(typeof(InvalidOperationException))]
+        //public void TestLecturerStatus()
+        //{
+        //    ManageUsers testUser = new ManageUsers();
+
+        //    LoginAction testLoginAction = new LoginAction("lnu", "119");
+        //    testLoginAction.AuthenticationCheck(testUser);
+        //    Assert.IsTrue(testLoginAction.AuthenticationCheck(testUser).Equals(AuthenticationStatus.Lecturer));
+        //}
+    }
+
+    [TestClass]
+    public class UnitTestsForSecurity
+    {
+        [TestMethod]
+        public void TestHashPasswordForEmptyPassword()
+        {
+            string password = string.Empty;
+            Assert.AreEqual(password, Security.HashPassword(password));
         }
 
         [TestMethod]
-        public void Test()
+        public void TestHashPasswordForNotEmptyPassword()
         {
-
-            ManageUsers testUser = new ManageUsers();
-
-            LoginAction testLoginAction = new LoginAction("isAbcenteHere", "111111");
-            testLoginAction.AuthenticationCheck(testUser);
-            Assert.IsTrue(testLoginAction.AuthenticationCheck(testUser).Equals(AuthenticationStatus.NoUser));
-            //ManageUsers testUser = new ManageUsers();
-            ////UserOfSite loggingUser = testUser.GetById("qwer");
-            ////testUser.AddUser(loggingUser);
-            //RegistrationAction user = new RegistrationAction();
-            //user.username = "qwer";
-            //user.password = "11111";
-            //user.email = "test@gmail.com";
-            //user.role = Role.Graduate;
-            //LoginAction testLoginAction = new LoginAction("qwer", "111111");
-            //Assert.IsFalse(testLoginAction.AuthenticationCheck(testUser).Equals(AuthenticationStatus.NoUser));
+            string password = "password";
+            Assert.AreEqual("e201065d0554652615c320c00a1d5bc8", Security.HashPassword(password));
         }
 
         [TestMethod]
-        [ExpectedException(typeof(InvalidOperationException))]
-        public void TestLecturerStatus()
+        public void TestHashPasswordForSamePasswords()
         {
-            ManageUsers testUser = new ManageUsers();
+            string password = "password";
+            Assert.AreEqual(Security.HashPassword(password), Security.HashPassword(password));
+        }
 
-            LoginAction testLoginAction = new LoginAction("lnu", "119");
-            testLoginAction.AuthenticationCheck(testUser);
-            Assert.IsTrue(testLoginAction.AuthenticationCheck(testUser).Equals(AuthenticationStatus.Lecturer));
+        [TestMethod]
+        public void TestHashPasswordForDifferentPasswords()
+        {
+            string password1 = "password";
+            string password2 = "Password";
+            Assert.AreNotEqual(Security.HashPassword(password1), Security.HashPassword(password2));
         }
     }
 }
