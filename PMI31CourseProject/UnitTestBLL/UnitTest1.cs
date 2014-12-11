38950484d8d952f65d482fd39fefe9c68dc88d23
molochiy <molochiy@gmail.com>
2014-12-11 21:46:00 +0200

    Євчакові тести

--- PMI31CourseProject/UnitTestBLL/UnitTest1.cs
+++ PMI31CourseProject/UnitTestBLL/UnitTest1.cs
@@ -8,6 +8,8 @@ using ProjectDatabase;
 using TypeMock;
 using TypeMock.ArrangeActAssert;
 using DAL;
+using ProjectDatabase;
+using System.Linq.Expressions;
 
 namespace BLLUnitTests
 {
@@ -82,13 +84,6 @@ namespace BLLUnitTests
             Assert.AreEqual(testAction.SetRegistrationStatus(), RegistrationStatus.RegistratedLecturer);
         }
 
-        [TestMethod]
-        public void TestMethodDefultConstructorForRegistrationAction()
-        {
-            RegistrationAction testAction = new RegistrationAction();
-            Assert.AreEqual(testAction.role, Role.Graduate);
-        }
-
         [TestMethod, Isolated]
         public void TestMethodRegistrationCheckIfFailed()
         {
@@ -113,7 +108,7 @@ namespace BLLUnitTests
             testAction.email = "test@gmail.com";
             testAction.fullName = "TestFullName";
             testAction.role = Role.Graduate;
-            User user = new User();
+            User user = new ProjectDatabase.User();
             ManageUsers fakeManageUser = Isolate.Fake.Instance<ManageUsers>();
             Isolate.Swap.AllInstances<ManageUsers>().With(fakeManageUser);
             Isolate.WhenCalled(() => fakeManageUser.GetById("login")).WithExactArguments().WillReturn(user);
@@ -131,7 +126,7 @@ namespace BLLUnitTests
             testAction.email = "test@gmail.com";
             testAction.fullName = "TestFullName";
             testAction.role = Role.Lecturer;
-            User user = new User();
+            User user = new ProjectDatabase.User();
             ManageUsers fakeManageUser = Isolate.Fake.Instance<ManageUsers>();
             Isolate.Swap.AllInstances<ManageUsers>().With(fakeManageUser);
             Isolate.WhenCalled(() => fakeManageUser.GetById("login")).WithExactArguments().WillReturn(user);
@@ -317,8 +312,145 @@ namespace BLLUnitTests
     }
 
     [TestClass]
+    public class UnitTestsForManageUsers
+    {
+        [TestMethod, Isolated]
+        public void TestMethodGetContacts()
+        {
+            UnitOfWork<User> unitOfWork = null;
+            IEnumerable<User> users = null;
+            UnitOfWork<User> fake = Isolate.Fake.Instance<UnitOfWork<User>>();
+            Isolate.Swap.AllInstances<UnitOfWork<User>>().With(fake);
+            Isolate.WhenCalled(() => fake.ContactRepository.GetAll()).WithExactArguments().WillReturn(null);
+            ManageUsers testUser = new ManageUsers();
+            Assert.IsTrue(users == testUser.GetContacts());
+        }
+
+        [TestMethod, Isolated]
+        public void TestMethodAddUser()
+        {
+            User user = new User();
+            UnitOfWork<User> unitOfWork = null;
+            UnitOfWork<User> fake = Isolate.Fake.Instance<UnitOfWork<User>>();
+            Isolate.Swap.AllInstances<UnitOfWork<User>>().With(fake);
+            Isolate.WhenCalled(() => fake.ContactRepository.Add(user)).WithExactArguments().IgnoreCall();
+            Isolate.WhenCalled(() => fake.Save()).WithExactArguments().IgnoreCall();
+            ManageUsers testUser = new ManageUsers();
+            Assert.IsTrue(testUser.AddUser(user));
+        }
+
+        [TestMethod, Isolated]
+        public void TestMethodUpdateUser()
+        {
+            User user = new User();
+            user.Password = "qwerty";
+            string login = string.Empty;
+            UnitOfWork<User> unitOfWork = null;
+            UnitOfWork<User> fake = Isolate.Fake.Instance<UnitOfWork<User>>();
+            Isolate.Swap.AllInstances<UnitOfWork<User>>().With(fake);
+            Isolate.WhenCalled(() => fake.ContactRepository.GetById(login)).WithExactArguments().WillReturn(user);
+            Isolate.WhenCalled(() => fake.Save()).WithExactArguments().IgnoreCall();
+            ManageUsers testUser = new ManageUsers();
+            Assert.IsTrue(testUser.UpdateUser(user, login));
+        }
+
+        [TestMethod, Isolated]
+        public void TestMethodDeleteUser()
+        {
+            User user = new User();
+            string login = string.Empty;
+            UnitOfWork<User> unitOfWork = null;
+            UnitOfWork<User> fake = Isolate.Fake.Instance<UnitOfWork<User>>();
+            Isolate.Swap.AllInstances<UnitOfWork<User>>().With(fake);
+            Isolate.WhenCalled(() => fake.ContactRepository.GetById(login)).WithExactArguments().WillReturn(user);
+            Isolate.WhenCalled(() => fake.ContactRepository.Delete(user)).WithExactArguments().IgnoreCall();
+            Isolate.WhenCalled(() => fake.Save()).WithExactArguments().IgnoreCall();
+            ManageUsers testUser = new ManageUsers();
+            Assert.IsTrue(testUser.DeleteUser(login));
+        }
+
+        [TestMethod, Isolated]
+        public void TestMethodGetIdByLogin()
+        {
+            User user = new User();
+            string login = string.Empty;
+            UnitOfWork<User> unitOfWork = null;
+            Expression<Func<User, bool>> expr = someUser => someUser.Login == login;
+            UnitOfWork<User> fake = Isolate.Fake.Instance<UnitOfWork<User>>();
+            Isolate.Swap.AllInstances<UnitOfWork<User>>().With(fake);
+            Isolate.WhenCalled(() => fake.ContactRepository.GetMany(expr)).WithExactArguments().WillReturn(null);
+            ManageUsers testUser = new ManageUsers();
+            Assert.IsTrue(-1 == testUser.GetIdByLogin(login));
+        }
+
+        [TestMethod, Isolated]
+        public void TestMethodGetById()
+        {
+            User user = new User();
+            string login = string.Empty;
+            user.Login = login;
+            long id = 0;
+            UnitOfWork<User> unitOfWork = null;
+            UnitOfWork<User> fake = Isolate.Fake.Instance<UnitOfWork<User>>();
+            Isolate.Swap.AllInstances<UnitOfWork<User>>().With(fake);
+            Isolate.WhenCalled(() => fake.ContactRepository.GetById(id)).WithExactArguments().WillReturn(user);
+            ManageUsers testUser = new ManageUsers();
+            Assert.IsTrue(user.Login == testUser.GetById(login).Login);
+        }
+
+        [TestMethod, Isolated]
+        public void TestMethodGetAllUsersByGraduateYear()
+        {
+            User user = new User();
+            int graduationYear = 0;
+            List<User> users = new List<User>();
+            UnitOfWork<User> unitOfWork = null;
+            Expression<Func<User, bool>> expressionForGetAllUsersByGraduateYear = someUser => someUser.UserInfo.GraduateInfo.GraduationYear == graduationYear;
+            UnitOfWork<User> fake = Isolate.Fake.Instance<UnitOfWork<User>>();
+            Isolate.Swap.AllInstances<UnitOfWork<User>>().With(fake);
+            Isolate.WhenCalled(() => fake.ContactRepository.GetMany(expressionForGetAllUsersByGraduateYear)).WithExactArguments().WillReturn(users);
+            ManageUsers testUser = new ManageUsers();
+            Assert.IsTrue(0 == testUser.GetAllUsersByGraduateYear(graduationYear).Count);
+        }
+
+        [TestMethod, Isolated]
+        public void TestMethodGetAllUsersByEntranceYear()
+        {
+            User user = new User();
+            int entranceYear = 0;
+            List<User> users = new List<User>();
+            UnitOfWork<User> unitOfWork = null;
+            Expression<Func<User, bool>> expressionForGetAllUsersByGraduateYear = someUser => someUser.UserInfo.GraduateInfo.EntranceYear == entranceYear;
+            UnitOfWork<User> fake = Isolate.Fake.Instance<UnitOfWork<User>>();
+            Isolate.Swap.AllInstances<UnitOfWork<User>>().With(fake);
+            Isolate.WhenCalled(() => fake.ContactRepository.GetMany(expressionForGetAllUsersByGraduateYear)).WithExactArguments().WillReturn(users);
+            ManageUsers testUser = new ManageUsers();
+            Assert.AreEqual(0, testUser.GetAllUsersByEntranceYear(entranceYear).Count);
+        }
+
+        [TestMethod, Isolated]
+        public void TestMethodGetAllUsersBySpeciality()
+        {
+            User user = new User();
+            string speciality = string.Empty;
+            List<User> users = new List<User>();
+            UnitOfWork<User> unitOfWork = null;
+            Expression<Func<User, bool>> expressionForGetAllUsersByGraduateYear = someUser => someUser.UserInfo.GraduateInfo.Speciality == speciality;
+            UnitOfWork<User> fake = Isolate.Fake.Instance<UnitOfWork<User>>();
+            Isolate.Swap.AllInstances<UnitOfWork<User>>().With(fake);
+            Isolate.WhenCalled(() => fake.ContactRepository.GetMany(expressionForGetAllUsersByGraduateYear)).WithExactArguments().WillReturn(users);
+            ManageUsers testUser = new ManageUsers();
+            Assert.IsTrue(0 == testUser.GetAllUsersBySpeciality(speciality).Count);
+        }
+
+    }
+
+    [TestClass]
     public class UnitTestsForSecurity
     {
+        /// <summary>
+        /// Testing hashing empty password
+        /// </summary>
         [TestMethod]
         public void TestHashPasswordForEmptyPassword()
         {
@@ -326,6 +458,9 @@ namespace BLLUnitTests
             Assert.AreEqual(password, Security.HashPassword(password));
         }
 
+        /// <summary>
+        /// Testing hashing some not empty password
+        /// </summary>
         [TestMethod]
         public void TestHashPasswordForNotEmptyPassword()
         {
@@ -333,6 +468,9 @@ namespace BLLUnitTests
             Assert.AreEqual("e201065d0554652615c320c00a1d5bc8", Security.HashPassword(password));
         }
 
+        /// <summary>
+        /// Testing hashing the same passwords
+        /// </summary>
         [TestMethod]
         public void TestHashPasswordForSamePasswords()
         {
@@ -340,6 +478,9 @@ namespace BLLUnitTests
             Assert.AreEqual(Security.HashPassword(password), Security.HashPassword(password));
         }
 
+        /// <summary>
+        /// Testing hashing the different passwords
+        /// </summary>
         [TestMethod]
         public void TestHashPasswordForDifferentPasswords()
         {
@@ -348,5 +489,4 @@ namespace BLLUnitTests
             Assert.AreNotEqual(Security.HashPassword(password1), Security.HashPassword(password2));
         }
     }
-
 }
